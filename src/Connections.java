6144c7d8673f6851444be599db2d98c4a94c7d50
Raymond-P <raymondperez158@gmail.com>
2017-11-18 14:50:00 -0500

    added documentation and methods

--- src/Connections.java
+++ src/Connections.java
@@ -10,6 +10,7 @@ import java.util.Hashtable;
  * TODO: find a way to broadcast in connect
  * TODO: store usernames as keys in list_of_messengers, we'll deal with repeated usernames later 
  * TODO: modify recieve thread to add an user when we recive their "ARP" reply
+ * "##### name-of-user #### AA.BB.CC.DD"
  * 
  * TODO: Add more documentation.
  * 
@@ -91,6 +92,35 @@ public class Connections {
 
 	}
 	
+	/**
+	 * Parses an IPv4 network address and parses it 
+	 * into a byte array. This is meant to be used 
+	 * when using the InetAddress.getByAddress() method
+	 * @param ipAddress A string representation of the IPv4 address
+	 * @return a byte array (of length 4) of representation of the ipAddress
+	 */
+	public byte[] parseIpToBytes(String ipAddress) {
+		// make the ip address into a byte array
+		byte[] address = new byte[4];
+		String[] ipArray = ipAddress.split("\\.");
+		System.out.println("address bytes: "+address);
+		System.out.println(ipAddress);
+		System.out.println(ipArray);
+		for( int i = 0; i < 4; i++){
+			address[i] = (byte) Integer.parseInt(ipArray[i]); 
+		}
+		return address;
+	}
+	
+	
+	/**
+	 * Sends a datagram packet to a host at the specified address
+	 * and at the specified port
+	 * @param message The string message to be sent
+	 * @param address the destination address in the form of a byte array
+	 * @param port	the port where the host will recieve the message
+	 * @deprecated
+	 */
 	public void send(String message, byte[] address, int port ) {
 		// TODO Auto-generated method stub
 		byte[] buffer = message.getBytes();
@@ -107,15 +137,48 @@ public class Connections {
 	}
 	
 	/**
+	 * Sends a datagram packet to a host at the specified address
+	 * and at the specified port
+	 * @param message The string message to be sent
+	 * @param address the destination address in the form of a String
+	 * @param port	the port where the host will recieve the message
+	 */
+	public void send(String message, String address, int port) {
+		// TODO Auto-generated method stub
+		byte[] parsedAddress = this.parseIpToBytes(address);
+		byte[] buffer = message.getBytes();
+		InetAddress destination_address;
+		try {
+			destination_address = InetAddress.getByAddress(parsedAddress);
+			DatagramPacket packet = new DatagramPacket(buffer, buffer.length, destination_address, port);
+			socket.send(packet);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/***
+	 * Sends a message inside a datagram packet to a
+	 * host at the specifed address
+	 * @param message the string message to be sent
+	 * @param address the destination addess in teh form of a String
+	 */
+	public void send(String message, String address) {
+		this.send(message, address, socketNumber); // the agreed upon socket was 64000
+	}
+	
+	/**
 	 * Connects to another client by specifying the client's username.
 	 * It does this by making an ARP-like request where it 
 	 * broadcasts the connect to username and it gets a response from that user.
 	 * the second part of this process, the message received, is handled in the 
 	 * receive method
 	 * @param username The name of the user that you are requesting to connect to
+	 * @param port the specified port that the host will recieve the message in.
 	 */
-	protected void connect(String username){
-		String port = "64000";
+	protected void connect(String username, String port){
 		System.out.println("Model connect was called... with username: "+username);
 		if (!list_of_messengers.containsKey(username)){
 			list_of_messengers.put(username+":"+port, new ClientGUI(this,username,port));
@@ -136,6 +199,18 @@ public class Connections {
 		
 	}
 	
+	/**
+	 * Connects to another client by specifying the client's username.
+	 * It does this by making an ARP-like request where it 
+	 * broadcasts the connect to username and it gets a response from that user.
+	 * the second part of this process, the message received, is handled in the 
+	 * receive method
+	 * @param username The name of the user that you are requesting to connect to.
+	 */
+	protected void connect(String username) {
+		this.connect(username, ""+socketNumber);
+	}
+	
 	
 	/**
 	 * Checks if we are already connected to someone with the specified
@@ -144,7 +219,6 @@ public class Connections {
 	 * @return True if its connected False if it is not
 	 */
 	protected boolean isConectedTo(String username) {
-		
 		return list_of_messengers.containsKey(username);
 	}
 	
